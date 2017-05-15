46878f3840c0ff530eca86df79faa76348d332af
Maciej Grzenda <maciej.grzenda@gmail.com>
2017-05-15 16:51:24 +0200

    Minor updates

--- src/labelprocessor/ProcessData.java
+++ src/labelprocessor/ProcessData.java
@@ -3,8 +3,9 @@ package labelprocessor;
 public class ProcessData {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
+		System.out.println("Just starting the implementation");
 	}
 
 }
